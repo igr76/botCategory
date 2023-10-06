bb649247e18d8af5ad0d0e73b3333976c1764d2f
igr76 <gritsuk766@gmail.com>
2023-10-07 08:53:01 +1000

    написание тестов

--- src/main/java/com/example/botCategory/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/botCategory/service/impl/CategoryServiceImpl.java
@@ -25,7 +25,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public String getCategoryPreviousLevel(int level) {
-       return "";//categoryRepository.findPreviousLevel(level);
+       return String.join("\n",categoryRepository.findPreviousLevel(level));
     }
 
     @Override
